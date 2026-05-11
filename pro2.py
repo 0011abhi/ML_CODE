67d5b874ef198995ad1c6660a623c458965c9374
Abhimanyu N Shettigar <166162912+0011abhi@users.noreply.github.com>
2026-05-11 22:56:14 +0530

    Update pro2.py

--- pro2.py
+++ pro2.py
@@ -3,15 +3,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_california_housing
 
-# Load dataset
 df = fetch_california_housing(as_frame=True).frame
 
-# Heatmap
 plt.figure(figsize=(10,8))
 sns.heatmap(df.corr(), annot=True, cmap="coolwarm")
 plt.title("Correlation Heatmap")
 plt.show()
 
-# Pairplot
 sns.pairplot(df[['MedInc', 'HouseAge', 'AveRooms', 'AveOccup', 'MedHouseVal']])
 plt.show()
