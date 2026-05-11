f0d0b7c13990be225d15ba03443a127a198c8787
Abhimanyu N Shettigar <166162912+0011abhi@users.noreply.github.com>
2026-05-11 22:55:15 +0530

    Update pro1.py

--- pro1.py
+++ pro1.py
@@ -2,24 +2,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_california_housing
 
-# Load dataset
 data = fetch_california_housing(as_frame=True)
 df = data.frame
 
-# Select numeric columns
 columns = df.select_dtypes(include=['float64', 'int64']).columns
 
-# Histograms
 df[columns].hist(figsize=(15,10), bins=30, edgecolor='black')
 plt.tight_layout()
 plt.show()
 
-# Boxplots
 df[columns].plot(kind='box', subplots=True, layout=(3,3), figsize=(15,10))
 plt.tight_layout()
 plt.show()
 
-# Outlier Detection using IQR
 print("Outliers Detection:")
 for col in columns:
     Q1 = df[col].quantile(0.25)
