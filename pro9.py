a1a71b9672cb3fa127830c96a6c072bb7d4e5423
Abhimanyu N Shettigar <166162912+0011abhi@users.noreply.github.com>
2026-05-11 22:59:58 +0530

    Fix missing plt.show() call at the end
    Added missing plt.show() to display images.

--- pro9.py
+++ pro9.py
@@ -3,29 +3,22 @@ from sklearn.datasets import fetch_olivetti_faces
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 
-# Load dataset
 data = fetch_olivetti_faces()
 
-# Features and labels
 X = data.images.reshape((400, -1))
 y = data.target
 
-# Split data
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Train Naive Bayes model
 model = GaussianNB()
 model.fit(X_train, y_train)
 
-# Accuracy
 print("Accuracy:", model.score(X_test, y_test))
 
-# Predict
 y_pred = model.predict(X_test)
 
-# Show sample images
 fig, axes = plt.subplots(2,5, figsize=(10,5))
 
 for i, ax in enumerate(axes.ravel()):
